7c909e1ef6ad6a10a04420723081035f5f54c0dc
reflash407 <reflash407@naver.com>
2026-03-20 13:58:02 +0900

    fix: 스프링 테스트 삭제

--- common/src/test/java/jabastore/common/CommonApplicationTests.java
+++ common/src/test/java/jabastore/common/CommonApplicationTests.java
@@ -3,7 +3,7 @@ package jabastore.common;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CommonApplicationTests {
 
     @Test
