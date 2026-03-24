5671a5086670942870a377c40a125b809c1007a0
Jeongha Park <162397816+mirupio@users.noreply.github.com>
2026-03-24 16:09:37 +0900

    docs: 주문에 대한 결제 준비 api swagger 설정

--- service/payment/src/main/java/jabaclass/payment/presentation/controller/PaymentController.java
+++ service/payment/src/main/java/jabaclass/payment/presentation/controller/PaymentController.java
@@ -4,6 +4,7 @@ import jabaclass.payment.application.usercase.PaymentUseCase;
 import jabaclass.payment.presentation.dto.request.PreparePaymentRequestDto;
 import jabaclass.payment.presentation.dto.response.PaymentResponseDto;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,14 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/payments")
-public class PaymentController {
+public class PaymentController implements PaymentApi {
 
-    private final PaymentUseCase paymentUseCase;
+	private final PaymentUseCase paymentUseCase;
 
-    @PostMapping("/prepare")
-    public ResponseEntity<PaymentResponseDto> prepare(@RequestBody PreparePaymentRequestDto request) {
-        PaymentResponseDto response = paymentUseCase.create(request);
+	@Override
+	@PostMapping("/prepare")
+	public ResponseEntity<PaymentResponseDto> preparePayment(@RequestBody PreparePaymentRequestDto request) {
+		PaymentResponseDto response = paymentUseCase.create(request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
 }
