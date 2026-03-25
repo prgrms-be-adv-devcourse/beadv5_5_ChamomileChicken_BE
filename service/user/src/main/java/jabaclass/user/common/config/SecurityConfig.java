2d77a06cdd7999e031faabe3d8cc36c3d2794112
JK-LEE98 <abc9541@gmail.com>
2026-03-25 11:47:59 +0900

    convention에 맞게 수정

--- service/user/src/main/java/jabaclass/user/common/config/SecurityConfig.java
+++ service/user/src/main/java/jabaclass/user/common/config/SecurityConfig.java
@@ -1,11 +1,7 @@
 package jabaclass.user.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jabaclass.auth.filter.JwtAuthenticationFilter;
-import jabaclass.auth.jwt.JwtProperties;
-import jabaclass.auth.jwt.JwtProvider;
-import jabaclass.auth.jwt.JwtTokenResolver;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,17 +14,47 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import jabaclass.auth.filter.JwtAuthenticationFilter;
+import jabaclass.auth.jwt.JwtProperties;
+import jabaclass.auth.jwt.JwtProvider;
+import jabaclass.auth.jwt.JwtTokenResolver;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @EnableWebSecurity
-@EnableConfigurationProperties(JwtProperties.class)
 @RequiredArgsConstructor
+@EnableConfigurationProperties(JwtProperties.class)
 public class SecurityConfig {
 
     private final JwtProperties jwtProperties;
 
     @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/api/v1/users/signup").permitAll()
+                .requestMatchers("/api/v1/users/login").permitAll()
+                .anyRequest().authenticated()
+        );
+
+        http.addFilterBefore(
+                new JwtAuthenticationFilter(jwtProvider(), tokenResolver(), objectMapper()),
+                UsernamePasswordAuthenticationFilter.class
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
@@ -42,33 +68,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement(session
-                        -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
-                // 권한 설정 (이메일 인증, 로그인, 회원가입은 모두 허용)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/users/signup").permitAll()
-                        .requestMatchers("/api/v1/users/login").permitAll() // 로그인 (추후 AuthController로 옮길 경우 수정)
-                        .anyRequest().authenticated()
-                )
-
-                .addFilterBefore(
-                        new JwtAuthenticationFilter(jwtProvider(), tokenResolver(), objectMapper()),
-                        UsernamePasswordAuthenticationFilter.class
-                );
-
-        return http.build();
-    }
-
 }
