e5f248e2d6b98cf63462d6459f602e261e7d0be1
reflash407 <reflash407@naver.com>
2026-03-26 03:36:00 +0900

    feat: User error code 추가

--- service/user/src/main/java/jabaclass/user/user/application/exception/UserErrorCode.java
+++ service/user/src/main/java/jabaclass/user/user/application/exception/UserErrorCode.java
@@ -9,7 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum UserErrorCode implements ErrorCode {
 
 	USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
-	DUPLICATE_EMAIL(HttpStatus.CONFLICT, "이미 사용 중인 이메일입니다.");
+	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 가입된 이메일입니다.");
 
 	private final HttpStatus status;
 	private final String message;
