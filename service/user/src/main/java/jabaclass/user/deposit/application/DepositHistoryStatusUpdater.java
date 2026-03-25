efccb53384b05e7af6953cf0e6fa5a99f5c91ff2
xuv2 <fish5478@naver.com>
2026-03-25 09:00:32 +0900

    fix: 사용하지 않는 DepositHistoryRepository 의존성 제거

--- service/user/src/main/java/jabaclass/user/deposit/application/DepositHistoryStatusUpdater.java
+++ service/user/src/main/java/jabaclass/user/deposit/application/DepositHistoryStatusUpdater.java
@@ -6,14 +6,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import jabaclass.user.deposit.domain.DepositHistory;
 import jabaclass.user.deposit.domain.DepositStatus;
-import jabaclass.user.deposit.domain.repository.DepositHistoryRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class DepositHistoryStatusUpdater {
-
-	private final DepositHistoryRepository depositHistoryRepository;
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void updateFailed(DepositHistory depositHistory) {
