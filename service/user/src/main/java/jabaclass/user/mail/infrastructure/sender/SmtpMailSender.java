90a785b52e1393f9633df13399b294b0608375f8
reflash407 <reflash407@naver.com>
2026-03-25 09:46:35 +0900

    feat : SmtpMailSender 예외 처리 세분화

--- service/user/src/main/java/jabaclass/user/mail/infrastructure/sender/SmtpMailSender.java
+++ service/user/src/main/java/jabaclass/user/mail/infrastructure/sender/SmtpMailSender.java
@@ -1,6 +1,7 @@
 package jabaclass.user.mail.infrastructure.sender;
 
 import org.springframework.context.annotation.Profile;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,12 @@ import jabaclass.user.mail.application.exception.MailErrorCode;
 import jabaclass.user.mail.domain.model.MailMessage;
 import jabaclass.user.mail.domain.sender.MailSender;
 import jabaclass.user.mail.infrastructure.config.MailProperties;
+import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Component
 @Profile({"dev", "prod"})
 @RequiredArgsConstructor
@@ -33,7 +37,8 @@ public class SmtpMailSender implements MailSender {
 			helper.setText(mailMessage.body(), mailMessage.html());
 
 			javaMailSender.send(mimeMessage);
-		} catch (Exception e) {
+		} catch (MessagingException | MailException e) {
+			log.error("메일 전송 실패. to={}, subject={}", mailMessage.to(), mailMessage.subject(), e);
 			throw new BusinessException(MailErrorCode.MAIL_SEND_FAILED);
 		}
 	}
