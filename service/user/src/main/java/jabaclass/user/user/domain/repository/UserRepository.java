9af610ceb2142f4dc503b1d1a4576b73b08c19ef
reflash407 <reflash407@naver.com>
2026-03-26 10:23:10 +0900

    fix: UserRepository 벌크 조회 메서드 추가

--- service/user/src/main/java/jabaclass/user/user/domain/repository/UserRepository.java
+++ service/user/src/main/java/jabaclass/user/user/domain/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package jabaclass.user.user.domain.repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,4 +19,6 @@ public interface UserRepository {
 	void delete(User user);
 
 	Optional<User> findByIdWithLock(UUID userId);
+
+	List<User> findAllByIds(List<UUID> userIds);
 }
