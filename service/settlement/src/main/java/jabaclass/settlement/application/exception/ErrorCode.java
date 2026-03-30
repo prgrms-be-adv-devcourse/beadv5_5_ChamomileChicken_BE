1a4427482006b6142a65eef00c32a94a60acdaa5
reflash407 <reflash407@naver.com>
2026-03-31 01:39:52 +0900

    feat: 정산 예외 응답 추가, GlobalExceptionHandler 등 추가

---
+++ service/settlement/src/main/java/jabaclass/settlement/application/exception/ErrorCode.java
@@ -0,0 +1,8 @@
+package jabaclass.settlement.application.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+	HttpStatus getStatus();
+	String getMessage();
+}
