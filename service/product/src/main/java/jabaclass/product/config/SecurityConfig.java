00e44827e5852cdc694b85d68bc330e7c52f80d8
choi38995 <choi38995@gmail.com>
2026-03-30 11:15:54 +0900

    feature : 내부 api 통신용 permitAll

--- service/product/src/main/java/jabaclass/product/config/SecurityConfig.java
+++ service/product/src/main/java/jabaclass/product/config/SecurityConfig.java
@@ -36,6 +36,8 @@ public class SecurityConfig {
 			.authorizeHttpRequests(auth ->
 				auth.requestMatchers(HttpMethod.GET, "/api/v1/products/**").permitAll()
 					.requestMatchers(HttpMethod.GET, "/api/v1/products/*/reviews/user").authenticated()
+					// api 통신용
+					.requestMatchers(HttpMethod.POST, "/api/v1/products/reservations").permitAll()
 					.requestMatchers("/swagger-ui/**")
 					.permitAll()
 					.requestMatchers("/swagger-ui.html")
