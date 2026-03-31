0937a0223ff0a3a6c7c45f041a3bc7f9b328b614
xuv2 <fish5478@naver.com>
2026-03-31 12:16:49 +0900

    fix: 환불 상태 REQUESTED에서 변경 되지 않았음 -> JPA save() 메서드 추가로 즉시 쓰기로 변경하여 버그 수정

--- service/payment/src/main/java/jabaclass/payment/application/service/PaymentService.java
+++ service/payment/src/main/java/jabaclass/payment/application/service/PaymentService.java
@@ -1,5 +1,11 @@
 package jabaclass.payment.application.service;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import jabaclass.payment.application.port.external.OrderPort;
 import jabaclass.payment.application.port.external.PaymentGatewayPort;
 import jabaclass.payment.application.port.external.UserPort;
@@ -8,21 +14,15 @@ import jabaclass.payment.domain.model.Payment;
 import jabaclass.payment.domain.model.Refund;
 import jabaclass.payment.domain.repository.PaymentRepository;
 import jabaclass.payment.domain.repository.RefundRepository;
-import jabaclass.payment.presentation.dto.request.PreparePaymentRequestDto;
+import jabaclass.payment.infrastructure.kafka.PaymentRefundCompletedEvent;
+import jabaclass.payment.infrastructure.kafka.PaymentRefundCompletedEventPublisher;
 import jabaclass.payment.presentation.dto.request.ConfirmPaymentRequestDto;
+import jabaclass.payment.presentation.dto.request.PreparePaymentRequestDto;
 import jabaclass.payment.presentation.dto.request.RefundPaymentRequestDto;
 import jabaclass.payment.presentation.dto.response.PaymentResponseDto;
 import jabaclass.payment.presentation.dto.response.RefundPaymentResponseDto;
-import jabaclass.payment.infrastructure.kafka.PaymentRefundCompletedEvent;
-import jabaclass.payment.infrastructure.kafka.PaymentRefundCompletedEventPublisher;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -155,7 +155,6 @@ public class PaymentService implements PaymentUseCase {
 			throw new IllegalStateException("결제 승인에 실패했습니다.", e);
 		}
 
-
 		// 결과 반환
 		return PaymentResponseDto.from(payment);
 	}
@@ -197,6 +196,7 @@ public class PaymentService implements PaymentUseCase {
 
 			payment.markCancelled();
 			refund.markCompleted();
+			refundRepository.save(refund);
 
 			refundCompletedEventPublisher.publish(
 				new PaymentRefundCompletedEvent(
@@ -206,6 +206,7 @@ public class PaymentService implements PaymentUseCase {
 			);
 		} catch (Exception e) {
 			refund.markFailed();
+			refundRepository.save(refund);
 			log.error("환불 실패. orderId={}, paymentId={}", payment.getOrderId(), payment.getId(), e);
 			throw new IllegalStateException("환불 처리에 실패했습니다.", e);
 		}
