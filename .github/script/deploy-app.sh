c8d7224aa86fd04d52507d9303725daee8a0d23e
choi38995 <choi38995@gmail.com>
2026-03-31 12:45:13 +0900

    feature : Dockerfile, deploy-app.sh 추가

---
+++ .github/script/deploy-app.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -e
+
+DOCKERHUB_USERNAME="${DOCKERHUB_USERNAME:?DOCKERHUB_USERNAME is required}"
+IMAGE_TAG="${IMAGE_TAG:?IMAGE_TAG is required}"
+SERVICE="${SERVICE:?SERVICE is required}"
+LATEST=latest
+
+BASE_DIR=/home/ubuntu/apps/data/service
+cd $BASE_DIR
+
+case $SERVICE in
+  user)
+    IMAGE="chamomile-user"
+    CONTAINER="user-service"
+    ;;
+  product)
+    IMAGE="chamomile-product"
+    CONTAINER="product-service"
+    ;;
+  order)
+    IMAGE="chamomile-order"
+    CONTAINER="order-service"
+    ;;
+  settlement)
+      IMAGE="chamomile-settlement"
+      CONTAINER="settlement-service"
+      ;;
+  payment)
+      IMAGE="chamomile-payment"
+      CONTAINER="payment-service"
+      ;;
+  file)
+      IMAGE="chamomile-file"
+      CONTAINER="file-service"
+      ;;
+  *)
+    echo "Unknown module"
+    exit 1
+    ;;
+esac
+
+  echo "Backing up current latest"
+  docker tag \
+    $DOCKERHUB_USERNAME/$IMAGE:latest \
+    $DOCKERHUB_USERNAME/$IMAGE:backup || true
+
+  echo "Pulling image: $DOCKERHUB_USERNAME/$IMAGE:$IMAGE_TAG"
+  docker pull $DOCKERHUB_USERNAME/$IMAGE:$IMAGE_TAG
+
+  echo "Tagging $IMAGE_TAG -> latest"
+  docker tag $DOCKERHUB_USERNAME/$IMAGE:$IMAGE_TAG $DOCKERHUB_USERNAME/$IMAGE:$LATEST  || true
+
+  echo "Restarting container: $CONTAINER"
+  docker compose up -d --force-recreate always $CONTAINER
+
+  echo "Done: $SERVICE"
