6722b83281c881b333c2e456d63479832c4ebd6d
JK-LEE98 <abc9541@gmail.com>
2026-04-09 13:29:35 +0900

    feat: RouteConfig 작성 완료 및 admin port 번호 수정

---
+++ api-gateway/src/main/java/jabaclass/apigateway/config/RouteConfig.java
@@ -0,0 +1,44 @@
+package jabaclass.apigateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			// File Service
+			.route("file-service", r -> r.path("/api/v1/files/**")
+				.uri("http://localhost:9000"))
+
+			// Payment Service
+			.route("payment-service", r -> r.path("/api/v1/payments/**")
+				.uri("http://localhost:9001"))
+
+			// Settlement Service
+			.route("settlement-service", r -> r.path("/api/v1/settlements/**")
+				.uri("http://localhost:9002"))
+
+			// User Service
+			.route("user-service", r -> r.path("/api/v1/auth/**", "/api/v1/users/**")
+				.uri("http://localhost:9003"))
+
+			// Product Service
+			.route("product-service", r -> r.path("/api/v1/products/**")
+				.uri("http://localhost:9004"))
+
+			// Order Service
+			.route("order-service", r -> r.path("/api/v1/orders/**")
+				.uri("http://localhost:9005"))
+
+			// Admin Service
+			.route("admin-service", r -> r.path("/api/v1/admins/**")
+				.uri("http://localhost:9007"))
+
+			.build();
+	}
+}
