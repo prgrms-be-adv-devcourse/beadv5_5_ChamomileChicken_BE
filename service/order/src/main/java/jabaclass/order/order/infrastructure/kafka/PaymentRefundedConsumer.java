d4155f622ae11acafad65065e05de99da381e547
Jeongha Park <162397816+mirupio@users.noreply.github.com>
2026-04-11 21:35:58 +0900

    refactor: order쪽에 PaymentRefundedEvent 및 Kafka consumer 수정

--- service/order/src/main/java/jabaclass/order/order/infrastructure/kafka/PaymentRefundedConsumer.java
+++ service/order/src/main/java/jabaclass/order/order/infrastructure/kafka/PaymentRefundedConsumer.java
@@ -10,18 +10,18 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class PaymentRefundCompletedConsumer {
+public class PaymentRefundedConsumer {
 
 	private final OrderUseCase orderUseCase;
 	private final ObjectMapper objectMapper;
 
 	@KafkaListener(
-		topics = "payment.refund.completed",
+		topics = "payment.refunded",
 		groupId = "order-service"
 	)
 	public void consume(String message) {
 		try {
-			PaymentRefundCompletedEvent event = objectMapper.readValue(message, PaymentRefundCompletedEvent.class);
+			PaymentRefundedEvent event = objectMapper.readValue(message, PaymentRefundedEvent.class);
 			orderUseCase.refund(event.orderId());
 		} catch (Exception e) {
 			throw new RuntimeException("환불 완료 이벤트 처리 실패", e);
--- service/order/src/main/java/jabaclass/order/order/infrastructure/kafka/PaymentRefundedEvent.java
+++ service/order/src/main/java/jabaclass/order/order/infrastructure/kafka/PaymentRefundedEvent.java
@@ -2,8 +2,8 @@ package jabaclass.order.order.infrastructure.kafka;
 
 import java.util.UUID;
 
-public record PaymentRefundCompletedEvent(
-	UUID orderId,
-	UUID productUserId
+public record PaymentRefundedEvent(
+	UUID paymentId,
+	UUID orderId
 ) {
 }
