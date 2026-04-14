a505ea57e4e4a144a0b9a36d9aa324c513d74ecc
maark1106 <mark1106@naver.com>
2026-04-14 16:41:02 +0900

    docs/refactor: Kafka 토픽 서비스별 단일 토픽으로 통합 및 문서화
    - 토픽을 Producer 서비스 기준 4개로 통합 (payment.events, order.events, product.events, deposit.events)
    - eventType 필드로 이벤트 종류 구분하는 구조로 설계
    - KafkaTopicConfig는 Producer 서비스에서만 토픽 생성하도록 정리
    - Order KafkaTopicConfig에서 Consumer 토픽 생성 제거
    - docs/kafka-topics.md 추가

--- service/order/src/main/java/jabaclass/order/common/config/KafkaTopicConfig.java
+++ service/order/src/main/java/jabaclass/order/common/config/KafkaTopicConfig.java
@@ -1,12 +1,5 @@
 package jabaclass.order.common.config;
 
-import jabaclass.order.infrastructure.kafka.OrderExpiredEventPublisher;
-import jabaclass.order.infrastructure.kafka.payment.PaymentCompletedEventConsumer;
-import jabaclass.order.infrastructure.kafka.payment.PaymentFailedEventConsumer;
-import jabaclass.order.infrastructure.kafka.payment.PaymentRefundCompletedEventConsumer;
-import jabaclass.order.infrastructure.kafka.product.OrderReservationConfirmedEventPublisher;
-import jabaclass.order.infrastructure.kafka.product.OrderReservationReleasedEventPublisher;
-import jabaclass.order.infrastructure.kafka.user.DepositRefundRequestedEventPublisher;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,57 +8,11 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaTopicConfig {
 
-	@Bean
-	public NewTopic paymentCompletedTopic() {
-		return TopicBuilder.name(PaymentCompletedEventConsumer.TOPIC)
-			.partitions(3)
-			.replicas(1)
-			.build();
-	}
+	public static final String TOPIC = "order.events";
 
 	@Bean
-	public NewTopic paymentFailedTopic() {
-		return TopicBuilder.name(PaymentFailedEventConsumer.TOPIC)
-			.partitions(3)
-			.replicas(1)
-			.build();
-	}
-
-	@Bean
-	public NewTopic paymentRefundCompletedTopic() {
-		return TopicBuilder.name(PaymentRefundCompletedEventConsumer.TOPIC)
-			.partitions(3)
-			.replicas(1)
-			.build();
-	}
-
-	@Bean
-	public NewTopic orderReservationConfirmedTopic() {
-		return TopicBuilder.name(OrderReservationConfirmedEventPublisher.TOPIC)
-			.partitions(3)
-			.replicas(1)
-			.build();
-	}
-
-	@Bean
-	public NewTopic orderReservationReleasedTopic() {
-		return TopicBuilder.name(OrderReservationReleasedEventPublisher.TOPIC)
-			.partitions(3)
-			.replicas(1)
-			.build();
-	}
-
-	@Bean
-	public NewTopic orderDepositRefundRequestedTopic() {
-		return TopicBuilder.name(DepositRefundRequestedEventPublisher.TOPIC)
-			.partitions(3)
-			.replicas(1)
-			.build();
-	}
-
-	@Bean
-	public NewTopic orderExpiredTopic() {
-		return TopicBuilder.name(OrderExpiredEventPublisher.TOPIC)
+	public NewTopic orderEventsTopic() {
+		return TopicBuilder.name(TOPIC)
 			.partitions(3)
 			.replicas(1)
 			.build();
