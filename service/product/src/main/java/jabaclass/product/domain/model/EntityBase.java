f3c791961ac5a40f52dfe5757039aefa0150391b
Jeongha Park <162397816+mirupio@users.noreply.github.com>
2026-04-21 16:38:39 +0900

    fix: EntityBase에 JPA Auditing 리스너 적용

--- service/product/src/main/java/jabaclass/product/domain/model/EntityBase.java
+++ service/product/src/main/java/jabaclass/product/domain/model/EntityBase.java
@@ -6,8 +6,10 @@ import java.util.UUID;
 import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AccessLevel;
@@ -19,6 +21,7 @@ import lombok.experimental.SuperBuilder;
 @Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public abstract class EntityBase {
 
 	@Id
