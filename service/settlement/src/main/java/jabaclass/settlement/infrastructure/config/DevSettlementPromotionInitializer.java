409bf2214ed4a9a26bba26a1e1ed42322d7329d5
reflash407 <reflash407@naver.com>
2026-04-24 16:06:47 +0900

    fix : 환경 설정 변경

--- service/settlement/src/main/java/jabaclass/settlement/infrastructure/config/DevSettlementPromotionInitializer.java
+++ service/settlement/src/main/java/jabaclass/settlement/infrastructure/config/DevSettlementPromotionInitializer.java
@@ -17,7 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-@Profile({"dev", "prod"})
+//@Profile({"dev", "prod"})
+@Profile("dev")
 @RequiredArgsConstructor
 public class DevSettlementPromotionInitializer implements ApplicationRunner {
 
